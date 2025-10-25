d0e84ed63ebce16a790a4a479c05b98c94c27991
Ben Waibel <waibel.ben@gmail.com>
2025-10-25 17:49:47 +0200

    refactor and add a error handling for the playeramount

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,28 @@
 package de.htwg.werwolf
 
+ import scala.io.StdIn.readLine
 
-def start(): Vector[String] = {
-    import scala.io.StdIn.readLine
+def getplayerAmount(): Int = {
     println("Willkommen zu Werwolf")
-    val Spieleranzahl = readLine("Bitte Spieleranzahl eingeben ( Mindestanzahl 2): ").toInt
+    val playerAmount = readLine("Bitte Spieleranzahl eingeben ( Mindestanzahl 2, Maximale Spieleranzahl 7): ").toInt
+    playerAmount
+}
 
-    val names = (0 until Spieleranzahl)
+def getPlayerNames(playerAmount : Int): Vector[String] = {
+    val names = (0 until playerAmount)
     .map(i => readLine(s"Spieler ${i+1}: Wie heißen sie: "))
     .toVector
-
     names
 }
 
-
 @main
   def main(): Unit = {
-    val player = start()
-    print(player);
-    //addRole(player)
+    val playerAmount = getplayerAmount()
+    if playerAmount < 2 || playerAmount > 6 then 
+      println("\u001b[31mUngültige Spieleranzahl\u001b[0m"); 
+      return;
+    val player = getPlayerNames(playerAmount)
+    val playerRoles= addRoles(player)
+    playerRoles.foreach((role, player) => println(s"${playerRoles(role)}"))
   }
 
