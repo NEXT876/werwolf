db70eded8f189c072aee3ebcbed5684e262eb1ed
Beat Haller <beat.m.haller@gmail.com>
2025-11-07 11:48:09 +0100

    fixed java singelton to scala functional style

--- src/main/scala/Votes.scala
+++ src/main/scala/Votes.scala
@@ -1,12 +1,10 @@
 package de.htwg.werwolf
 
-object Votes:
-  private var votes: Map[String, Int] = Map.empty
+class Votes(initialVotes: Map[String, Int] = Map.empty):
 
   def addVote(player: String): Unit =
     val current = votes.getOrElse(player, 0)
     votes = votes.updated(player, current + 1)
-
 
   def getVotedPlayer: String =
     votes.maxBy(_._2)._1
