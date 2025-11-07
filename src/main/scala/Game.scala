926a579f3b6da1addcff1cc918999ab7e99b6a5c
Ben Waibel <waibel.ben@gmail.com>
2025-11-07 10:17:47 +0100

    installled auto formatter : in user-settings.json : "[scala]": {     "editor.defaultFormatter": "scalameta.metals"   }

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,78 +1,73 @@
 package de.htwg.werwolf
 
 enum Roles:
-    case werwolf
-    case villager
-    case amor
-    case terrorist
-    case witch
+  case werwolf
+  case villager
+  case amor
+  case terrorist
+  case witch
 
-    def toPlayer (name : String) : Player = this match
-        case Roles.werwolf  => Werwolf(name)
-        case Roles.villager => Villager(name)
-        case Roles.amor => Amor(name)
-        case Roles.terrorist => Terrorist(name)
-        case Roles.witch => Witch(name)
-    
-def addRoles(players : Vector[String]) : Map[String, Player] = {
-    import scala.util.Random
-    //val playerRoles : Map[String, Player] = Map()
+  def toPlayer(name: String): Player = this match
+    case Roles.werwolf   => Werwolf(name)
+    case Roles.villager  => Villager(name)
+    case Roles.amor      => Amor(name)
+    case Roles.terrorist => Terrorist(name)
+    case Roles.witch     => Witch(name)
 
-    if players.size == 2 then  // sorgt dafür dass wenn es nur 2 Spieler gibt es immer 1 Werwolf und 1 Villager sind(sonst unnötig)
-        val roles = Vector(Roles.werwolf, Roles.villager)
-        val shuffeledRoles = Random.shuffle(roles)
-        (players zip shuffeledRoles)
-        .map { case (name, role) =>
-            val player = role.toPlayer(name)
-            player.name -> player
-        }.toMap
+def addRoles(players: Vector[String]): Map[String, Player] = {
+  import scala.util.Random
+  // val playerRoles : Map[String, Player] = Map()
 
-    else
-        val werwolfAmount = if players.size <= 3 then 1 else 2
-        val roles = Vector(Roles.villager, Roles.witch, Roles.amor, Roles.terrorist)
-        val shuffeledRoles = Random.shuffle(roles)
-        val finalRoles : Vector[Roles] = (Vector.fill(werwolfAmount)(Roles.werwolf)
-        ++ shuffeledRoles).take(players.size)  
-        (players zip finalRoles)
-        .map {case (name, role) =>
-            val player = role.toPlayer(name)
-            player.name -> player
-        }.toMap
+  if players.size == 2
+  then // sorgt dafür dass wenn es nur 2 Spieler gibt es immer 1 Werwolf und 1 Villager sind(sonst unnötig)
+    val roles = Vector(Roles.werwolf, Roles.villager)
+    val shuffeledRoles = Random.shuffle(roles)
+    (players zip shuffeledRoles).map { case (name, role) =>
+      val player = role.toPlayer(name)
+      player.name -> player
+    }.toMap
+  else
+    val werwolfAmount = if players.size <= 3 then 1 else 2
+    val roles = Vector(Roles.villager, Roles.witch, Roles.amor, Roles.terrorist)
+    val shuffeledRoles = Random.shuffle(roles)
+    val finalRoles: Vector[Roles] = (Vector.fill(werwolfAmount)(Roles.werwolf)
+      ++ shuffeledRoles).take(players.size)
+    (players zip finalRoles).map { case (name, role) =>
+      val player = role.toPlayer(name)
+      player.name -> player
+    }.toMap
 }
 
-def night (playerRoles : Map[String, Player]) : Map[String, Player] = {
-    import scala.io.StdIn.readLine
-    import scala.io.Source
+def night(playerRoles: Map[String, Player]): Map[String, Player] = {
+  import scala.io.StdIn.readLine
+  import scala.io.Source
 
-    val text =
+  val text =
     Source.fromResource("narrator_Night.txt").getLines().mkString("\n")
 
-    tiping(text, 30)
+  tiping(text, 30)
 
-    val updatedRoles =  playerRoles.foldLeft(playerRoles) {
-        case (currentState, (name, player)) => 
-        if(player.role == "Werwolf" && player.isAlive){
-            print(printPlayerRoles(currentState)) 
+  val updatedRoles = playerRoles.foldLeft(playerRoles) { case (currentState, (name, player)) =>
+    if (player.role == "Werwolf" && player.isAlive) {
+      print(printPlayerRoles(currentState))
 
-            val vote = readLine(s"Spieler $name ,bitte geben sie an wen sie umbringen möchten:")
-            val voteText = player.vote(currentState(vote))
-            print(s"\u001b[31m${voteText}\u001b[0m\n")
-            Votes.addVote(vote)
-            currentState           
-        }else{
-            currentState
-        }
+      val vote = readLine(s"Spieler $name ,bitte geben sie an wen sie umbringen möchten:")
+      val voteText = player.vote(currentState(vote))
+      print(s"\u001b[31m${voteText}\u001b[0m\n")
+      Votes.addVote(vote)
+      currentState
+    } else {
+      currentState
     }
-    val votedPlayer = Votes.getVotedPlayer
-    playerRoles(votedPlayer).die
-    /// weitere rollen
-    ///...
-    ///.. 
-   
+  }
+  val votedPlayer = Votes.getVotedPlayer
+  playerRoles(votedPlayer).die
+  /// weitere rollen
+  /// ...
+  /// ..
 
-
-    ///Dorfbewohner
-    ///...
-    ///..
-    updatedRoles
+  /// Dorfbewohner
+  /// ...
+  /// ..
+  updatedRoles
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,12 @@ package de.htwg.werwolf
 import scala.io.StdIn.readLine
 import java.util.concurrent.atomic.AtomicInteger
 
-object GlobalDayCounter{
+object GlobalDayCounter {
   private val day = new AtomicInteger(0)
 
-  def increment() : Int = day.incrementAndGet()
-  def get() : Int = day.get()
-  def reset() : Unit = day.set(0)
+  def increment(): Int = day.incrementAndGet()
+  def get(): Int = day.get()
+  def reset(): Unit = day.set(0)
 }
 
 /*
@@ -22,59 +22,57 @@ object GlobalDayCounter{
   "\u001b[4m"   // Unterstrichen
 )
 reset = "\u001b[0m"
-*/
+ */
 
-def getplayerAmount(test : Boolean, fakeInput : Int): Int = {
-    val playerAmount = 
-      if test then
-       fakeInput
-      else
-        readLine("Bitte Spieleranzahl eingeben ( Mindestanzahl 2, Maximale Spieleranzahl 7): ").toInt
-    playerAmount
+def getplayerAmount(test: Boolean, fakeInput: Int): Int = {
+  val playerAmount =
+    if test then fakeInput
+    else
+      readLine("Bitte Spieleranzahl eingeben ( Mindestanzahl 2, Maximale Spieleranzahl 7): ").toInt
+  playerAmount
 }
 
-def getPlayerNames(test : Boolean, playerAmount : Int, fakeInput : Vector[String]): Vector[String] = {
-      if test then
-      fakeInput
-      else
-        (0 until playerAmount)
-        .foldLeft(Set[String]()) { (acc, i) =>
-          var name = readLine(s"Spieler ${i+1} bitte geben sie ihren Namen an: ").trim
-           while(acc.contains(name)){
-            println("Name gibt es bereits, bitte wähle einen anderen")
-            name = readLine(s"Spieler ${i+1} bitte geben sie ihren Namen an: ").trim
-          } 
+def getPlayerNames(test: Boolean, playerAmount: Int, fakeInput: Vector[String]): Vector[String] = {
+  if test then fakeInput
+  else
+    (0 until playerAmount)
+      .foldLeft(Set[String]()) { (acc, i) =>
+        var name = readLine(s"Spieler ${i + 1} bitte geben sie ihren Namen an: ").trim
+        while (acc.contains(name)) {
+          println("Name gibt es bereits, bitte wähle einen anderen")
+          name = readLine(s"Spieler ${i + 1} bitte geben sie ihren Namen an: ").trim
+        }
 
-          acc + name
-        }.toVector
+        acc + name
+      }
+      .toVector
 }
 
-def tiping(text : String, waitTime_ms : Int) :Boolean = {
+def tiping(text: String, waitTime_ms: Int): Boolean = {
   text.foreach { buchstabe =>
-    if(buchstabe == '.'){
-      Thread.sleep(waitTime_ms*4)
+    if (buchstabe == '.') {
+      Thread.sleep(waitTime_ms * 4)
       print(buchstabe)
-    }else{
+    } else {
       print(buchstabe)
       Thread.sleep(waitTime_ms)
     }
   }
   println()
-  return true
+  true
 }
 
 @main
-  def main(): Unit = {
-    tiping("Willkommen zu Werwolf", 100)
-    val playerAmount = getplayerAmount(false, 0)
-    if playerAmount < 2 || playerAmount > 6 then
-      println("\u001b[31mUngültige Spieleranzahl\u001b[0m");
-      return;
-    val player = getPlayerNames(false, playerAmount, Vector[String]())
-    val playerRoles= addRoles(player)
-    println(printPlayerRoles(playerRoles))
+def main(): Unit = {
+  tiping("Willkommen zu Werwolf", 100)
+  val playerAmount = getplayerAmount(false, 0)
+  if playerAmount < 2 || playerAmount > 6 then
+    println("\u001b[31mUngültige Spieleranzahl\u001b[0m");
+    return;
+  val player = getPlayerNames(false, playerAmount, Vector[String]())
+  val playerRoles = addRoles(player)
+  println(printPlayerRoles(playerRoles))
 
-    val updatedplayerroles = night(playerRoles)
-    println(printPlayerRoles(updatedplayerroles))
-  }
-
+  val updatedplayerroles = night(playerRoles)
+  println(printPlayerRoles(updatedplayerroles))
+}
