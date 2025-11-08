9ec61fb1e4b79f25ba677e90e8057b66ace35f2d
Ben Waibel <waibel.ben@gmail.com>
2025-11-08 12:45:50 +0100

    adding random Narrator Texte

--- build.sbt
+++ build.sbt
@@ -11,6 +11,9 @@ lazy val root = project
     libraryDependencies ++= Seq(
       "org.scalameta" %% "munit" % "1.0.0" % Test,
       "org.scalactic" %% "scalactic" % "3.2.18",
-      "org.scalatest" %% "scalatest" % "3.2.18" % Test
+      "org.scalatest" %% "scalatest" % "3.2.18" % Test,
+      "com.lihaoyi" %% "upickle" % "4.0.0",
+      "com.lihaoyi" %% "os-lib" % "0.9.1"
+
   )
 )
---
+++ src/main/scala/narrator.scala
@@ -0,0 +1,33 @@
+package de.htwg.werwolf
+
+import upickle.default._
+
+case class Night(
+    Start: List[String],
+    Werwolf: List[String],
+    Amor: List[String],
+    Witch: List[String]
+)
+
+case class Root(Night: Night)
+
+object Night {
+  implicit val rw: ReadWriter[Night] = macroRW
+}
+
+object Root {
+  implicit val rw: ReadWriter[Root] = macroRW
+}
+
+def randomNarratorText(Rolle: String): String =
+  val jsonString = os.read(os.pwd / "src" / "main" / "resources" / "narrator.json")
+  val data = read[Root](jsonString)
+  val text = Rolle match {
+    case "Start"   => data.Night.Start
+    case "Werwolf" => data.Night.Werwolf
+    case "Witch"   => data.Night.Witch
+    case "Amor"    => data.Night.Amor
+    case _         => List[String]("")
+  }
+  val randomText = util.Random.shuffle(text).head
+  randomText
